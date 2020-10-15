cd0b7971b6c046882f9ec5c42cc3bd1d114382a1
J-Mont07 <72569343+J-Mont07@users.noreply.github.com>
2020-10-15 12:40:33 -0500

    Add files via upload

---
+++ Module_1_lab_activity.py
@@ -0,0 +1,50 @@
+#Module 1 Lab Activity
+#
+#Juan Montoya
+#
+#October 1, 2020
+#
+#Examples:
+#
+#This line evaluates to 90 because subtracting 10 from 100 gives you 90.
+(100 - 10)
+
+#This line evaluates to true because 100 is greater than 10.
+(100 > 10)
+
+#Worth 10 points each:
+
+#This line will evaluate to 4 because it is adding 2 + 2.
+print (2 + 2)
+
+#This line will evaluate to 3.0 because it is 12 is being divided by 4.
+print (12/4)
+
+#This line evaluates to 0 because 8 modulo 2 is evenly didable. 
+print (8%2)
+
+#This line evaluates to 696 because of Order Operation first mutiples 100 by 7, and then it subtracts 4.
+print (100*7-4)
+
+#This line evaluates to 300 because of Order of Operartion. First subtract 7-4,which gives you 3. Then multiplty by 100.
+print (100*(7-4))
+
+#This line evaluates to 5 because of Order of operation. Add 8 and 9. Find the modulo of 80 with 17. And then subtract by 5. 
+print (80%(8+9)-7)
+
+#This line evaluates to True. 10 is greater than 7.
+print (10 > 7)
+
+#This line evaluates to True. 20 is less than equall too 20.
+print (20 <= 20)
+
+#This line evaluates to False. 20 is not equal to 30. 10 does not equal 7.
+print (20 != 30 and 10 == 7)
+
+#This line evaluates to True because 100 > 100 is false. 7%2 == 1 which is True.
+print (100 > 98+2 or 7%2 == 1)
+
+
+
+#BONUS: Modify this code to display the result of each line to the user through the console when this file is run
+#       (there are several ways to do this).
